905e389d527595bf35589304f6c1825b87eec964
hadroncollider7 <chaulo74@students.rowan.edu>
2021-11-24 19:50:44 -0500

    Update with ADC configuration

--- WiFi_TemperatureMonitor/main.c
+++ WiFi_TemperatureMonitor/main.c
@@ -8,8 +8,39 @@ int main(void)
 {
     WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
 
+    /*************************************************************************
+    * SETUP ADC
+    * The conversion will be stored in the ADC12MEM0 register.
+    * Note: The ADC12IEs and ADC12IFGs corresponds to the MEMx registers, not
+    *   the ADC input channel.
+    * Using module ADC12 A0
+    * Input channel: 6
+    * Test: send data to wifi
+    *************************************************************************/
     /************************************************************************
-     * SEUP UP USCI REGISTERS
+     * Configure ports
+     ************************************************************************/
+    P6SEL |= BIT6;      // Port 6.6 set to analog function
+    P6DIR &= ~BIT6;     // Configure p6.6 as input
+
+
+    //************* SET CONTROL REGISTERS ***************
+    REFCTL0 &= ~BIT7;     // clear REFMSTR bit to give control of the reference generator to ADC12
+    ADC12CTL0 |= ADC12REF2_5V + ADC12REFON + ADC12ON + ADC12SHT0_2;     // SHT = 16 cycles
+    ADC12CTL0 |= ADC12ENC;                                              // enable conversion
+    ADC12CTL1 |= ADC12SHS0 + ADC12SSEL1 + ADC12SHP;
+        // select ACLK
+        // sample hold source is ADC12SC bit
+        // ample and hold pulse mode
+
+    ADC12MCTL6 |= ADC12INCH_6;      /* Analog 6 input channel select */
+    ADC12IFG &= ~ADC12IFG0;         /* Clear ADC12MEM0 interrupt request flag */
+    ADC12IE |= ADC12IE0;            /* Enable interrupt for the ADC12MEM0 register */
+
+
+
+    /************************************************************************
+     * SETUP UP USCI REGISTERS
      * Data format: 115200N81
      * Low frequency mode
      * BRCLK ~ 1 Mhz
@@ -19,20 +50,20 @@ int main(void)
      ***********************************************************************/
     P3SEL |= BIT3 + BIT4;   /* configure p3.3, p3.4 to function as tx,rx */
 
-    /* CONFIGURE CONTROL REGISTERS */
+    /* Configure control registers */
     UCA0CTL1 |= UCSWRST;    /* enable configuration of control registers */
     UCA0CTL1 |= UCSSEL_2;   /* BRCLK = SMCLK */
 
 
     /* ********************************************************************
-     * CONFIGURE THE BAUD RATE FOR UART
+     * Configure the baud rate for UART
      * 115200
      * UCOS16 = 0 (low-frequency mode)
      * UCBRX = 9
      * UCBRSX = 1
      * ********************************************************************/
     UCA0BR0 = 0x9;          /* low-byte BRCLK prescaler */
-    UCA0MCTL |= UCBRS_1;   /* sets BRS */
+    UCA0MCTL |= UCBRS_1;    /* sets BRS */
 
 
     /*************************************************************************
@@ -41,43 +72,23 @@ int main(void)
      *************************************************************************/
      UCA0CTL1 &= ~UCSWRST;     /* Enable USCI*/
 
-     UCA0TXBUF = 0x66;
+//     UCA0TXBUF = 0x66;
 
-  /************************************************************************
-   * CONFIGURE PORTS
-   ************************************************************************/
-//  P6SEL |= BIT6;     // Port 6.6 set to analog function
-//  P6DIR &= ~BIT6;     // Configure p6.6 as input
-//
-//
-//  //************* SET CONTROL REGISTERS ***************
-//  REFCTL0 &= ~BIT7;     // clear REFMSTR bit to give control of the reference generator to ADC12
-//  ADC12CTL0 |= ADC12REF2_5V + ADC12REFON + ADC12ON + ADC12SHT0_2; // SHT = 16 cycles
-//  ADC12CTL0 |= ADC12ENC;  // enable conversion
-//  ADC12CTL1 |= ADC12SHS0 + ADC12SSEL1 + ADC12SHP;
-//                  // select ACLK
-//                  // sample hold source is ADC12SC bit
-//                  // ample and hold pulse mode
-//
-//  ADC12MCTL6 |= ADC12INCH_6;      // analog 6 input channel select
-//    ADC12IE |= BIT6;        // enable interrupt for ADDC12IFG6
-//
-//
-//  //************ ENABLE INTERRUPTS AND START CONVERSION ********************
-//  while(1)
-//  {
-//      ADC12CTL0 |= ADC12SC;                  // start conversion
-//
-//      __bis_SR_register(GIE);
-//  }
+     //************ ENABLE INTERRUPTS AND START CONVERSION ********************
+     ADC12CTL0 |= ADC12SC;          // start conversion
 
+     __bis_SR_register(GIE);
+     __no_operation();              /* for debugger */
 
     return 0;
 }
 
-//******************** ADC12 ISR *****************************************
-//#pragma vector = ADC12_VECTOR
-//__interrupt void ADC12_ISR(void)
-//{
-//
-//}
+/**************************************************************************
+ *  ADC12 ISR
+ ***************************************************************************/
+#pragma vector = ADC12_VECTOR
+__interrupt void ADC12_ISR(void)
+{
+    UCA0TXBUF = 0xA;
+    ADC12IE &= ~ADC12IFG0;
+}
